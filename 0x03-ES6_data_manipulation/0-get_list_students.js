1795593047e2c5e69ba94304ddc5b05243c548b8
aliceada96 <aliceada96@gmail.com>
2024-01-09 20:11:25 +0300

    Basic array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
   return [
     { id: 1, firstname: 'Guillaume', location: 'San Fransisco' },
     { id: 2, firstname: 'James', location: 'Columbia' },
-    { id: 5, firstname: 'Seerena', location: 'San Fransico' },
+    { id: 5, firstname: 'Seerena', location: 'San Fransisco' },
   ];
 }
