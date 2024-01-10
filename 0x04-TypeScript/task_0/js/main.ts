0fa3a2890397465b76ebbbbcfcdf815d8a8df15e
aliceada96 <aliceada96@gmail.com>
2024-01-10 23:31:55 +0300

    interrface creation

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -5,18 +5,22 @@ interface Student {
     location: string;
 }
 
-const student1: Student = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-    location: 'New York'
-};
+let studentsList: Student[] = [
+    {firstName: 'John', lastName: 'Doe', age: 15, location: 'New York'},
+    {firstName: 'Jane', lastName: 'Doe', age: 15, location: 'New York'},
+];
 
-const student2: Student = {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 25,
-    location: 'New York'
-};
+function generateTable(table: HTMLTableElement, data: Student[]) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (const key in studentsList) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(studentsList[key].toString());
+      cell.appendChild(text);
+    }
+  }
+}
 
-const studentsList: Student = [student1, student2];
+let table = document.querySelector('table')!;
+let data = Object.keys(studentsList[0]);
+generateTable(table, studentsList);
