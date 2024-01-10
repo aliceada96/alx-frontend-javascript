3057a8075bfcd4597c94c6aa767e932e2c2d4b1b
aliceada96 <aliceada96@gmail.com>
2024-01-10 10:04:46 +0300

    clean seet

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(inputSet, startString) {
+  if (startString === '') {
+    return '';
+  }
   const valuesArray = [...inputSet];
   const filteredValues = valuesArray.filter((value) => value.startsWith(startString));
   const resultString = filteredValues.map((value) => value.slice(startString.length)).join('-');
